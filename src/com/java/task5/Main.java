0c447c5f103e14865c4e052682acb85640e8cf92
Aleksandr Moskalchuk <moskalchuk.aleksandr@gmail.com>
2022-10-30 01:20:24 +0300

    my comment

--- src/com/java/task5/Main.java
+++ src/com/java/task5/Main.java
@@ -1,14 +1,21 @@
 package com.java.task5;
 
 public class Main {
+
+    public static final String HELLO = "hello";
+
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+
+        extracted();
+
+    }
+
+    private static void extracted() {
         System.out.println("Hello world");
         System.out.println("Hello world");
     }
