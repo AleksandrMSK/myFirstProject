2c564defef416ada8bcce178ba20d88096a4adad
Aleksandr Moskalchuk <moskalchuk.aleksandr@gmail.com>
2022-10-30 00:52:55 +0300

    test

--- src/com/java/lesson4/Main.java
+++ src/com/java/lesson4/Main.java
@@ -2,6 +2,35 @@ package com.java.lesson4;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
