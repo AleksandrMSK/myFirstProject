6e49026d4f6775f9af95f01b4c0d4d79247328d8
Aleksandr Moskalchuk <moskalchuk.aleksandr@gmail.com>
2022-10-30 01:16:01 +0300

    my comment

---
+++ src/com/java/task5/Main.java
@@ -0,0 +1,15 @@
+package com.java.task5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
